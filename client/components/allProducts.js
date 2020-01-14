52ebf60d53eb0eb547eed933ea954aac8bfea45a
srosenstock <sorah.rosenstock@gmail.com>
2020-01-14 15:26:15 -0500

    can view all products

--- client/components/allProducts.js
+++ client/components/allProducts.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import {getAllProducts} from '../store/reducer'
+import {connect} from 'react-redux'
 
-export class allProducts extends React.Component {
-  // constructor(){
-  //     super()
-  // }
+export class AllProducts extends React.Component {
+  //   constructor(){
+  //       super()
+  //   }
+
+  componentDidMount() {
+    this.props.getAllProducts()
+  }
 
   render() {
-    return <h1>hi</h1>
+    let products = this.props.products
+
+    return (
+      <div>
+        <h1>Products:</h1>{' '}
+        {products &&
+          products.map(product => (
+            <div key={product.id}>
+              <h3>{product.title}</h3>
+              <img src={product.imageUrl} />
+              <h4>{product.price}</h4>
+            </div>
+          ))}
+      </div>
+    )
   }
 }
+
+const mapStateToProps = state => ({
+  products: state.products
+})
+
+const mapDispatchToProps = dispatch => ({
+  getAllProducts: () => dispatch(getAllProducts())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllProducts)
