a9c2a68850cab9332ea00bfbdaaf48138f1569e4
srosenstock <sorah.rosenstock@gmail.com>
2020-01-14 16:20:09 -0500

    added cart component

--- client/app.js
+++ client/app.js
@@ -1,16 +1,27 @@
 import React from 'react'
-
+import {BrowserRouter, Route} from 'react-router-dom'
 import {Navbar} from './components'
 import Routes from './routes'
 import AllProducts from './components/AllProducts'
+import {Cart} from './components/cart'
+// import CheckoutForm from './components/CheckoutForm'
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Routes />
-      <AllProducts />
-    </div>
+    <BrowserRouter>
+      <div>
+        {/* <div> */}
+        {/* <Navbar />
+          // <Routes /> */}
+        {/* <AllProducts /> */}
+        {/* </div> */}
+        <Route path="/" component={Navbar} />
+        <Route path="/" component={Routes} />
+        <Route exact path="/" component={AllProducts} />
+        <Route exact path="/cart" component={Cart} />
+        {/* <Route path='/cart/checkout' component={CheckoutForm} /> */}
+      </div>
+    </BrowserRouter>
   )
 }
 
