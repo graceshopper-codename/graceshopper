954b35253896686eca4dbf54e862ca23966ff5e4
ChloeJ <chloe.jandsten@gmail.com>
2020-01-14 14:16:46 -0500

    Order Model complete & added items to seed

--- server/db/models/products.js
+++ server/db/models/products.js
@@ -11,7 +11,7 @@ const Products = db.define('products', {
   type: {
     type: Sequelize.ENUM(
       'deckbuilding',
-      'worker placement',
+      'strategy',
       'family',
       'party',
       'cooperative'
@@ -33,41 +33,3 @@ const Products = db.define('products', {
 })
 
 module.exports = Products
-
-// /**
-//  * instanceMethods
-//  */
-// User.prototype.correctPassword = function(candidatePwd) {
-//   return User.encryptPassword(candidatePwd, this.salt()) === this.password()
-// }
-
-// /**
-//  * classMethods
-//  */
-// User.generateSalt = function() {
-//   return crypto.randomBytes(16).toString('base64')
-// }
-
-// User.encryptPassword = function(plainText, salt) {
-//   return crypto
-//     .createHash('RSA-SHA256')
-//     .update(plainText)
-//     .update(salt)
-//     .digest('hex')
-// }
-
-// /**
-//  * hooks
-//  */
-// const setSaltAndPassword = user => {
-//   if (user.changed('password')) {
-//     user.salt = User.generateSalt()
-//     user.password = User.encryptPassword(user.password(), user.salt())
-//   }
-// }
-
-// User.beforeCreate(setSaltAndPassword)
-// User.beforeUpdate(setSaltAndPassword)
-// User.beforeBulkCreate(users => {
-//   users.forEach(setSaltAndPassword)
-// })
