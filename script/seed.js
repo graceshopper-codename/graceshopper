2ae055da7082c8701ec38fda900886d75996d419
ChloeJ <chloe.jandsten@gmail.com>
2020-01-14 12:05:17 -0500

    Product Model Created, and one item seeded

--- script/seed.js
+++ script/seed.js
@@ -2,6 +2,7 @@
 
 const db = require('../server/db')
 const {User} = require('../server/db/models')
+const {Products} = require('../server/db/models')
 
 async function seed() {
   await db.sync({force: true})
@@ -13,6 +14,20 @@ async function seed() {
   ])
 
   console.log(`seeded ${users.length} users`)
+
+  const products = await Promise.all([
+    Products.create({
+      title: 'Everdell',
+      type: 'worker placement',
+      description:
+        'Within the charming valley of Everdell, beneath the boughs of towering trees, among meandering streams and mossy hollows, a civilization of forest critters is thriving and expanding. From Everfrost to Bellsong, many a year have come and gone, but the time has come for new territories to be settled and new cities established. You will be the leader of a group of critters intent on just such a task. There are buildings to construct, lively characters to meet, events to host—you have a busy year ahead of yourself. Will the sun shine brightest on your city before the winter moon rises?',
+      price: 20.15,
+      imageUrl:
+        'https://cf.geekdo-images.com/itemrep/img/WDbT-w4bNeIwxojLW92I_SO5GSU=/fit-in/246x300/pic3918905.png'
+    })
+  ])
+  console.log(`seeded ${products.length} products`)
+
   console.log(`seeded successfully`)
 }
 
