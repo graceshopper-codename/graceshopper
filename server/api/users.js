7533f70850df77e868c06718248972aebe11feb3
Emily <esfendler@gmail.com>
2020-01-16 12:31:07 -0500

    feat: only admins can see all users api

--- server/api/users.js
+++ server/api/users.js
@@ -4,24 +4,27 @@ module.exports = router
 
 //console.log req.user
 const isUser = (req, res, next) => {
-  if (req.user) {
+  console.log(req.user, 'user')
+  if (req.user.isAdmin) {
     try {
+      console.log(req.user, 'user')
       return next()
     } catch (err) {
       next(err)
     }
-    res.redirect('/')
   } else res.redirect('/')
 }
 
 router.get('/', isUser, async (req, res, next) => {
   try {
+    console.log('test')
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
       attributes: ['id', 'email']
     })
+
     res.json(users)
   } catch (err) {
     next(err)
