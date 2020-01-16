8d05c1463b25ea7880752250ca019f51117cec02
Emily <esfendler@gmail.com>
2020-01-16 12:37:51 -0500

    chore: remove console.logs

--- server/api/users.js
+++ server/api/users.js
@@ -2,12 +2,9 @@ const router = require('express').Router()
 const {User} = require('../db/models')
 module.exports = router
 
-//console.log req.user
-const isUser = (req, res, next) => {
-  console.log(req.user, 'user')
+const isAdmin = (req, res, next) => {
   if (req.user.isAdmin) {
     try {
-      console.log(req.user, 'user')
       return next()
     } catch (err) {
       next(err)
@@ -15,16 +12,14 @@ const isUser = (req, res, next) => {
   } else res.redirect('/')
 }
 
-router.get('/', isUser, async (req, res, next) => {
+router.get('/', isAdmin, async (req, res, next) => {
   try {
-    console.log('test')
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
       attributes: ['id', 'email']
     })
-
     res.json(users)
   } catch (err) {
     next(err)
