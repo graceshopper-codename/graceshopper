70a54db822898f9f8ba40683d038c178d3bb3f07
srosenstock <sorah.rosenstock@gmail.com>
2020-01-21 11:13:10 -0500

    bug: fixed the cart itmes and order history

--- client/components/cartTotal.js
+++ client/components/cartTotal.js
@@ -1,32 +1,32 @@
-import React from 'react'
-import {getCart} from '../store/cart'
-import {connect} from 'react-redux'
+// import React from 'react'
+// import {getCart} from '../store/cart'
+// import {connect} from 'react-redux'
 
-export class CartTotal extends React.Component {
-  componentDidMount() {
-    this.props.getCart()
-  }
+// export class CartTotal extends React.Component {
+//   componentDidMount() {
+//     this.props.getCart()
+//   }
 
-  render() {
-    let total = 0
-    let cart = this.props.cart
-    if (cart) {
-      for (let i = 0; i < cart.length; i++) {
-        total += cart[i].quantity
-      }
-      return <div> {total} </div>
-    } else {
-      return <div> {total} </div>
-    }
-  }
-}
+//   render() {
+//     let total = 0
+//     let cart = this.props.cart
+//     if (cart) {
+//       for (let i = 0; i < cart.length; i++) {
+//         total += cart[i].quantity
+//       }
+//       return <div> {total} </div>
+//     } else {
+//       return <div> {total} </div>
+//     }
+//   }
+// }
 
-const mapStateToProps = state => ({
-  cart: state.cart
-})
+// const mapStateToProps = state => ({
+//   cart: state.cart
+// })
 
-const mapDispatchToProps = dispatch => ({
-  getCart: () => dispatch(getCart())
-})
+// const mapDispatchToProps = dispatch => ({
+//   getCart: () => dispatch(getCart())
+// })
 
-export default connect(mapStateToProps, mapDispatchToProps)(CartTotal)
+// export default connect(mapStateToProps, mapDispatchToProps)(CartTotal)
