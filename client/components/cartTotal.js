c7503e2c8b73ac71c39b204483b67fd78f6b94b9
srosenstock <sorah.rosenstock@gmail.com>
2020-01-20 22:56:32 -0500

    feature: can view total number of items in the cart

---
+++ client/components/cartTotal.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {getCart} from '../store/cart'
+import {connect} from 'react-redux'
+
+export class CartTotal extends React.Component {
+  componentDidMount() {
+    this.props.getCart()
+  }
+
+  render() {
+    let total = 0
+    let cart = this.props.cart
+    for (let i = 0; i < cart.length; i++) {
+      total += cart[i].quantity
+    }
+    return <h1>{total}</h1>
+  }
+}
+
+const mapStateToProps = state => ({
+  cart: state.cart
+})
+
+const mapDispatchToProps = dispatch => ({
+  getCart: () => dispatch(getCart())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartTotal)
